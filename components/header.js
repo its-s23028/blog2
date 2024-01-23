1852f3005347c73e601e18064f605cfb1a4b0ebc
its-s23028 <s23028@std.it-college.ac.jp>
2024-01-23 08:20:19 +0000

    66p
    Next/LinkのLinkコンポーネントでページ間のリンクを設定する

--- components/header.js
+++ components/header.js
@@ -1,4 +1,12 @@
+import Logo from 'components/logo'
+import Nav from 'components/nav'
+
 const Header = () => {
-  return <header>HEADER</header>
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  )
 }
 export default Header
---
+++ components/logo.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Logo = () => {
+  return (
+    <Link href='/'>
+      CUBE
+    </Link>
+  )
+}
+export default Logo
