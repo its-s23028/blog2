8c7d230bb58768087d7beb7ab9af918fbf2e5b71
its-s23028 <s23028@std.it-college.ac.jp>
2024-01-23 07:41:33 +0000

    56p
    カスタムAppコンポーネンツでLayoutをすヴェ手のペーっじに入れる

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,6 @@
-import Header from 'components/header'
 import Hero from 'components/hero'
-import Footer from 'components/footer'
 
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
-  )
+  return <Hero />
 }
 export default Home
