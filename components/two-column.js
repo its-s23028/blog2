fff4843c7a049fa0e833406dde5b8fc860802968
its-s23028 <s23028@std.it-college.ac.jp>
2024-01-31 06:35:52 +0000

    128p
    3つのコンポーネントで二段組みのレイアウとを構成する

---
+++ components/two-column.js
@@ -0,0 +1,14 @@
+import styles from 'styles/two-column.module.css'
+
+const TwoColumn = ({ children }) => {
+  return <div className={styles.flexContainer}>{children}</div>
+}
+
+const TwoColumnMain = ({ children }) => {
+  return <div className={styles.main}>{children}</div>
+}
+
+const TwoColumnSidebar = ({ children }) => {
+  return <div className={styles.sidebar}>{children}</div>
+}
+export { TwoColumn, TwoColumnMain, TwoColumnSidebar }
