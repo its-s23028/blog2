21a2abaac371a0b145728e1ad79d36d361eda732
its-s23028 <s23028@std.it-college.ac.jp>
2024-02-20 07:59:12 +0000

    272p
    記事一覧にNext/imageでアイキャッチ画像
    を追加する。

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,9 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
 
 const Blog = ({ posts }) => {
   return (
@@ -16,6 +19,15 @@ const Blog = ({ posts }) => {
 
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const imageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
