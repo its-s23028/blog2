ed8f31f4e05629fb516a98590f5934e231ed7ed3
its-s23028 <s23028@std.it-college.ac.jp>
2024-02-20 06:53:04 +0000

    268p
    記事一覧を作成する.

---
+++ pages/api/revalidate.js
@@ -0,0 +1,13 @@
+const handler = async (req, res) => {
+  if (req.query.secret !== process.env.SECRET_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' })
+  }
+  try {
+    await res.unstable_revalidate('/blog/shedule')
+    await res.unstable_revalidate('/blog/music')
+    return res.json({ revalidated: true })
+  } catch (err) {
+    return res.status(500).send('Error revalidating')
+  }
+}
+export default handler
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,5 +1,6 @@
 import { getPostBySlug, getAllSlugs } from 'lib/api'
 import { extractText } from 'lib/extract-text'
+import { prevNextPost } from 'lib/prev-next-post'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
@@ -11,6 +12,7 @@ import {
 } from 'components/two-column'
 import ConvertBody from 'components/convert-body'
 import PostCategories from 'components/post-categories'
+import Pagination from 'components/pagination'
 import Image from 'next/legacy/image'
 import { eyecatchLocal } from 'lib/constants'
 import { getPlaiceholder } from 'plaiceholder'
@@ -22,7 +24,9 @@ const Post = ({
   content,
   eyecatch,
   categories,
-  description
+  description,
+  prevPost,
+  nextPost
 }) => {
   return (
     <Container>
@@ -61,17 +65,23 @@ const Post = ({
             <PostCategories categories={categories} />
           </TwoColumnSidebar>
         </TwoColumn>
+        <Pagination
+          prevText={prevPost.title}
+          prevUrl={`/blog/${prevPost.slug}`}
+          nextText={nextPost.title}
+          nextUrl={`/blog/${nextPost.slug}`}
+        />
       </article>
     </Container>
   )
 }
 
 export async function getStaticPaths () {
-  const allSlugs = await getAllSlugs()
+  const allSlugs = await getAllSlugs(5)
 
   return {
     paths: allSlugs.map(({ slug }) => `/blog/${slug}`),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -79,23 +89,31 @@ const getStaticProps = async context => {
   const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
+  if (!post) {
+    return { notFound: true }
+  } else {
+    const description = extractText(post.content)
 
-  const description = extractText(post.content)
+    const eyecatch = post.eyecatch ?? eyecatchLocal
 
-  const eyecatch = post.eyecatch ?? eyecatchLocal
+    const imageBuffer = await getImageBuffer(eyecatch.url)
+    const { base64 } = await getPlaiceholder(imageBuffer)
+    eyecatch.blurDataURL = base64
 
-  const imageBuffer = await getImageBuffer(eyecatch.url)
-  const { base64 } = await getPlaiceholder(imageBuffer)
-  eyecatch.blurDataURL = base64
+    const allSlugs = await getAllSlugs()
+    const [prevPost, nextPost] = prevNextPost(allSlugs, slug)
 
-  return {
-    props: {
-      title: post.title,
-      publish: post.publishDate,
-      content: post.content,
-      eyecatch: eyecatch,
-      categories: post.categories,
-      description: description
+    return {
+      props: {
+        title: post.title,
+        publish: post.publishDate,
+        content: post.content,
+        eyecatch: eyecatch,
+        categories: post.categories,
+        description: description,
+        prevPost: prevPost,
+        nextPost: nextPost
+      }
     }
   }
 }
