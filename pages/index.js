42104cffd3cfa17548132570df06bfa1c8c30a9a
its-s23028 <s23028@std.it-college.ac.jp>
2024-02-21 05:43:56 +0000

    276p
    トップページに記事一覧を表示する。

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
+import { getAllPosts } from 'lib/api'
 import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
+import { getImageBuffer } from 'lib/getImageBuffer'
 
-const Home = () => {
+const Home = ({ posts }) => {
   return (
     <Container>
       <Meta />
       <Hero title='CUBE' subtitle='アウトプットしてくサイト' imageOn />
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
+
+export async function getStaticProps () {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const ImageBuffer = await getImageBuffer(post.eyecatch.url)
+    const { base64 } = await getPlaiceholder(ImageBuffer)
+    post.eyecatch.blurDataURL = base64
+  }
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
+
 export default Home
